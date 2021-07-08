68fd04513e0d2bf83046ceb8123f5a4259f19d91
swkidd <kiddsoftware@gmail.com>
2021-07-08 16:40:25 +0900

    refactor, remove latlng

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
-const aws = require('aws-sdk');
-const fileType = require('file-type')
-const { "v4": uuidv4 } = require('uuid');
-const sharp = require('sharp');
+const aws = require("aws-sdk");
+const fileType = require("file-type");
+const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
 
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
 const dynamo = new aws.DynamoDB.DocumentClient();
-const MARKER_DB = "memnut-markers"
-const MEMAGE_DB = "memnut-memages"
-const MEM_DB = "memnut-mems"
-const VALID_BUCKET = "memnut-valid-images"
+const MARKER_DB = "memnut-markers";
+const MEMAGE_DB = "memnut-memages";
+const MEM_DB = "memnut-mems";
+const VALID_BUCKET = "memnut-valid-images";
 
-const validMimeType = type => type === "image/jpeg" || type === "image/png" || type === "image/webp"
-const validType = type => type === "marker" || type === "mem" || type === "memage"
+const validMimeType = (type) =>
+  type === "image/jpeg" || type === "image/png" || type === "image/webp";
+const validType = (type) =>
+  type === "marker" || type === "mem" || type === "memage";
 
-exports.handler = async event => {
+exports.handler = async (event) => {
   const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
   try {
     var params = {
       Bucket: bucket,
       Key: key,
-      Range: "bytes=0-4100"
+      Range: "bytes=0-4100",
     };
-    const { ContentType, Body, Metadata } = await s3.getObject(params).promise();
+    const { ContentType, Body, Metadata } = await s3
+      .getObject(params)
+      .promise();
 
-    const image_id = uuidv4()
-    const type = Metadata.type
-    const creator = JSON.parse(Metadata.creator)
+    const image_id = uuidv4();
+    const type = Metadata.type;
+    const creator = JSON.parse(Metadata.creator);
 
     const mimeType = await fileType.fromBuffer(Body);
     if (!validMimeType(ContentType) || !validMimeType(mimeType.mime)) {
-      throw new Error('Invalid upload mime type');
+      throw new Error("Invalid upload mime type");
     } else if (!validType(type)) {
-      throw new Error('Invalid upload type');
+      throw new Error("Invalid upload type");
     } else {
-
       var getparams = {
         Bucket: bucket,
         Key: key,
-      }
+      };
       const getresp = await s3.getObject(getparams).promise();
-      const raw_img = getresp.Body
+      const raw_img = getresp.Body;
 
-      const image_key = `${image_id}.webp`
+      const image_key = `${image_id}.webp`;
       // const image_key_md = `${image_id}_md.webp`
       // const image_key_sm = `${image_id}_sm.webp`
 
-      const webp_image = await sharp(raw_img)
-        .webp()
-        .toBuffer()
+      let webp_image;
+      if (mimeType.mime !== "image/webp") {
+        webp_image = await sharp(raw_img).webp().toBuffer();
+      } else {
+        webp_image = raw_img;
+      }
 
-      const promises = []
+      const promises = [];
 
-      promises.push(s3.putObject({
-        Bucket: VALID_BUCKET,
-        Key: image_key,
-        Body: webp_image,
-        ContentType: 'image/webp'
-      }).promise());
+      promises.push(
+        s3
+          .putObject({
+            Bucket: VALID_BUCKET,
+            Key: image_key,
+            Body: webp_image,
+            ContentType: "image/webp",
+          })
+          .promise()
+      );
 
       // promises.push(s3.putObject({
       //   Bucket: VALID_BUCKET,
@@ -82,80 +94,39 @@ exports.handler = async event => {
       //   ContentType: 'image/webp'
       // }).promise());
 
-      await Promise.all(promises)
+      await Promise.all(promises);
+
+      let db;
+      const item = {
+        image_key,
+        creator,
+        email: key,
+      };
 
       if (type === "marker") {
-        const markerid = Metadata.markerid
-        const latlng = JSON.parse(Metadata.latlng)
-        const getResp = await dynamo
-          .get({
-            TableName: MARKER_DB,
-            Key: {
-              id: markerid
-            }
-          })
-          .promise();
-
-        if (getResp.Item) {
-          const marker = getResp.Item
-          const item = {
-            ...marker,
-            image_keys: [...marker.image_keys, image_key]
-          }
-
-          await dynamo
-            .put({
-              TableName: MARKER_DB,
-              Item: item
-            })
-            .promise();
-        } else {
-          const item = {
-            id: markerid,
-            latlng,
-            image_keys: [image_key],
-            creator,
-            email: key,
-          }
-          await dynamo
-            .put({
-              TableName: MARKER_DB,
-              Item: item
-            })
-            .promise();
-        }
+        const markerid = Metadata.markerid;
+        item["marker_id"] = markerid;
+        db = MARKER_DB;
       } else if (type === "memage") {
-        const item = {
-          id: uuidv4(),
-          image_keys: [image_key],
-          creator,
-          email: key,
-        }
-        await dynamo
-          .put({
-            TableName: MEMAGE_DB,
-            Item: item
-          })
-          .promise();
+        const memageid = Metadata.memageid;
+        item["memage_id"] = memageid;
+        db = MEMAGE_DB;
       } else if (type === "mem") {
-        const markerid = Metadata.markerid
-        const item = {
-          id: uuidv4(),
-          marker_id: markerid,
-          image_key,
-          creator,
-          email: key,
-        }
-        await dynamo
-          .put({
-            TableName: MEM_DB,
-            Item: item
-          })
-          .promise();
+        const memid = Metadata.memid;
+        const memageid = Metadata.memageid;
+        item["memage_id"] = memageid;
+        item["mem_id"] = memid;
+        db = MEM_DB;
       }
+
+      await dynamo
+        .put({
+          TableName: db,
+          Item: item,
+        })
+        .promise();
     }
-  }
-  catch (err) {
-    return "Error validatingobject"
+  } catch (err) {
+    return "Error validatingobject";
   }
 };
